29fe41ad9d496b4354c5bb2151c026e1f910f2bb
Katie Vogel <kvog983@gmail.com>
2019-08-30 13:35:43 -0400

    duplicates stopped

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-var connection = mongoose.connection; 
+var connection = mongoose.connection;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -45,25 +45,37 @@ app.post("/scrape", function (req, res) {
             var result = {};
             result.title = ch(this).children("a").text();
             result.link = ch(this).children("a").attr("href");
+            results.push(result);
+            console.log(result)
+        });
 
-            if(results.indexOf(result.link) == -1){
-                results.push(result);
-            }
-        });
-        db.Article.updateOne(result.title, result.link, {upsert: true}).then(function (dbArticle) {
-            var hbsParams = {
-                articles: dbArticle
-            }
-            console.log(dbArticle);
-            res.render("index", hbsParams);
-        });
+        // 2.
+        var promises = []
+        for (var i = 0; i < results.length; i++){
+            var result = results[i];
+            var p = db.Article.updateOne({ link: result.link }, result, {upsert: true});
+            promises.push(p);
+        }
         
+        // 3.
+        var finishedResult = Promise.all(promises);
+        finishedResult.then(function (objectsUpdated) {  
+            var articlesCursor = connection.db.collection('articles').find({});
+            var articles = articlesCursor.toArray().then(function (articles) {
+                console.log("final list of articles" + JSON.stringify(articles));
+                res.render("index", { articles: articles});
+            });
+        })
+
+    }).catch(function (err) {
+        res.json(err);
     });
+
 });
 
 app.put("/notes/:id", function (req, res) {
-    console.log(req.body); 
-    db.Article.findOneAndUpdate({ _id: req.params.id }, { note: req.body.body}, { new: true }).then(function (dbArticle) {
+    console.log(req.body);
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { note: req.body.body }, { new: true }).then(function (dbArticle) {
         console.log("dbArticle:" + dbArticle);
         res.json(dbArticle);
     }).catch(function (err) {
